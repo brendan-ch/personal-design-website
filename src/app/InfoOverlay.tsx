816ff40a45a1791183b968c94e6bfa3183c5684e
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 11:23:09 -0700

    add component skeleton

---
+++ src/app/InfoOverlay.tsx
@@ -0,0 +1,33 @@
+import styles from './InfoOverlay.module.css';
+
+interface Props {
+  visible: boolean,
+  onClose: () => any,
+  children?: React.ReactNode,
+}
+
+/**
+ * Generic info overlay which displays as a full page on mobile.
+ * @param props
+ * @returns
+ */
+export default function InfoOverlay({
+  visible,
+  onClose,
+  children,
+}: Props) {
+  return (
+    // Invisible wrapper containing the overlay content
+    <div
+      className={visible ? `${styles.container} ${styles.containerVisible}` : styles.container}
+      onClick={onClose}
+    >
+      {/* On mobile, display full page */}
+      {/* On desktop display overlay */}
+      <div className={styles.contentContainer}>
+        {/* Children component, including any action buttons and anchors */}
+        {children}
+      </div>
+    </div>
+  )
+}
