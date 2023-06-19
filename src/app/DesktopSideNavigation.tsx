33526d679b05c79dd4a495f8b5cb12b76fbffb44
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 13:03:27 -0700

    fix formatting

--- src/app/DesktopSideNavigation.tsx
+++ src/app/DesktopSideNavigation.tsx
@@ -20,28 +20,28 @@ export default function DesktopSideNavigation({
 
   return (
     <div className={styles.container}>
-        <InfoOverlay
-          onClose={() => setOverlayVisible(false)}
-          visible={overlayVisible}
-        >
-          <Anchor text="A note from Brendan" />
-          <p>I’m Brendan, a designer and developer in Orange, CA. Welcome to my website!</p>
-          <p>This place is a creative outlet for me to showcase my design works. If you’d like, feel free to check out my other projects, or learn more about what I do.</p>
-          <p>Happy exploring!</p>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 8,
-          }}>
-            <ActionButton
-              highlighted
-              text="Start exploring"
-            />
-            <ActionButton
-              text="Learn more about me"
-            />
-          </div>
-        </InfoOverlay>
+      <InfoOverlay
+        onClose={() => setOverlayVisible(false)}
+        visible={overlayVisible}
+      >
+        <Anchor text="A note from Brendan" />
+        <p>I’m Brendan, a designer and developer in Orange, CA. Welcome to my website!</p>
+        <p>This place is a creative outlet for me to showcase my design works. If you’d like, feel free to check out my other projects, or learn more about what I do.</p>
+        <p>Happy exploring!</p>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 8,
+        }}>
+          <ActionButton
+            highlighted
+            text="Start exploring"
+          />
+          <ActionButton
+            text="Learn more about me"
+          />
+        </div>
+      </InfoOverlay>
       {/* Standalone logo */}
       <LogoStandalone
         width={72}
