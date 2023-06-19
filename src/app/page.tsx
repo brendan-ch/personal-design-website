0f2b517042c78ee3b1d81855e475d006e3d517df
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 12:36:51 -0700

    add layout skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import styles from './page.module.css'
 import InfoOverlay from './InfoOverlay'
 import Anchor from './Anchor'
@@ -8,28 +6,6 @@ import ActionButton from './ActionButton'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <InfoOverlay
-        onClose={() => {}}
-        visible
-      >
-        <Anchor text="A note from Brendan" />
-        <p>I’m Brendan, a designer and developer in Orange, CA. Welcome to my website!</p>
-        <p>This place is a creative outlet for me to showcase my design works. If you’d like, feel free to check out my other projects, or learn more about what I do.</p>
-        <p>Happy exploring!</p>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 8,
-        }}>
-          <ActionButton
-            highlighted
-            text="Start exploring"
-          />
-          <ActionButton
-            text="Learn more about me"
-          />
-        </div>
-      </InfoOverlay>
       <p>Some page content</p>
       <p>Some page content</p>
       <p>Some page content</p>
