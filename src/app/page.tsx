4fb6bb86e69f82496b9215ed37fc8e76abab19ee
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 16:14:27 -0700

    add proper metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import styles from './page.module.css'
+
+export const metadata: Metadata = {
+  title: 'Featured Works',
+  description: 'Featured works on the design.bchen.dev website.'
+}
 
 export default function Home() {
   return (
