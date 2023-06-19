65ff3bfee47b881c03aa140d52a83edcbd3cdfdc
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 15:58:33 -0700

    add some links

--- src/app/WelcomeOverlay.tsx
+++ src/app/WelcomeOverlay.tsx
@@ -3,6 +3,7 @@
 import InfoOverlay from "@/common/InfoOverlay"
 import Anchor from "@/common/Anchor"
 import ActionButton from "@/common/ActionButton"
+import Link from "next/link"
 
 interface Props {
   visible: boolean,
@@ -23,7 +24,7 @@ export default function WelcomeOverlay({
     >
       <Anchor text="A note from Brendan" />
       <p>I’m Brendan, a designer and developer in Orange, CA. Welcome to my website!</p>
-      <p>This place is a creative outlet for me to showcase my design works. If you’d like, feel free to check out my other projects, or learn more about what I do.</p>
+      <p>This place is a creative outlet for me to showcase my design works. If you’d like, feel free to check out <a rel="noreferrer" target="_blank" href="https://bchen.dev/work">my other projects</a>, or <a rel="noreferrer" target="_blank" href="https://bchen.dev">learn more about what I do</a>.</p>
       <p>Happy exploring!</p>
       <div style={{
         display: 'flex',
