31835aabc7403345a7f10fa6372728623103e3d8
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 14:41:48 -0700

    add placeholder pages

---
+++ src/app/graphic-design/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function GraphicDesign() {
+  return (
+    <main>
+
+      <p>This is the Graphic Design page</p>
+    </main>
+  );
+}
---
+++ src/app/wip/page.tsx
@@ -0,0 +1,7 @@
+export default function WIP() {
+  return (
+    <main>
+      <p>This is the WIP page</p>
+    </main>
+  );
+}
