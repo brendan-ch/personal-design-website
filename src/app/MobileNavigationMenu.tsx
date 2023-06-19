6e35c372a4f0d006f611c9988d09e614b687bca9
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 15:08:46 -0700

    remove jsLoaded state

---
+++ src/app/MobileNavigationMenu.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styles from './MobileNavigationMenu.module.css';
+import Footer from './Footer';
+import MobileNavigationBar from './MobileNavigationBar';
+
+interface ButtonProps {
+  toggled?: boolean,
+  text: string,
+  href: string,
+}
+
+export function MobileNavMenuButton({ toggled, text, href }: ButtonProps) {
+  return (
+    (<Link href={href} className={styles.buttonWrapper}>
+
+      <div className={styles.buttonTextContainer}>
+        <h1 className={toggled ? styles.red : undefined}>
+          <b>
+            {text}
+          </b>
+        </h1>
+      </div>
+
+    </Link>)
+  );
+}
+
+interface Props {
+  selected?: string,
+  visible?: boolean,
+  onClose?: () => any,
+}
+
+/**
+ * Navigation menu that displays a list of links.
+ * @param props
+ * @returns
+ */
+export default function MobileNavMenu({ selected, visible, onClose }: Props) {
+  return (
+    <div
+      className={!visible ? `${styles.container} ${styles.containerInvisible}` : `${styles.container} ${styles.containerVisible}`}
+      role="menu"
+    >
+      <div className={styles.contentContainer}>
+        <MobileNavigationBar
+          style={{
+            zIndex: 4,
+            position: 'relative',
+          }}
+          mobileButtonType="close"
+          onMobileButtonClick={onClose}
+          hideLogo
+        />
+        <div className={styles.buttons}>
+          <MobileNavMenuButton
+            toggled={selected === 'Home'}
+            text="Featured Works"
+            href="/"
+          />
+          <MobileNavMenuButton
+            toggled={selected === 'My Work'}
+            text="UI/UX Design"
+            href="/work"
+          />
+          {/* to-do: emphasize this button */}
+          <MobileNavMenuButton
+            toggled={selected === 'Contact Me'}
+            text="Graphic Design"
+            href="/contact"
+          />
+        </div>
+        <Footer
+          onNoteOpen={() => {}}
+        />
+      </div>
+    </div>
+  );
+}
