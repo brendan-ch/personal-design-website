744cb92bd708f11b52574dafb68a3e075528e3a6
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 15:21:41 -0700

    simplify functionality

--- src/app/MobileNavigationBar.tsx
+++ src/app/MobileNavigationBar.tsx
@@ -1,20 +1,16 @@
 'use client'
 
 import Link from 'next/link';
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import styles from './MobileNavigationBar.module.css';
+import utils from './utils.module.css';
 import Exit from '../icons/Exit';
 import Hamburger from '../icons/Hamburger';
 import LogoStandalone from '../icons/LogoStandalone';
+import MobileNavigationMenu from './MobileNavigationMenu';
 
 interface Props {
-  /**
-   * @deprecated No longer used since 2022-11-11.
-   */
-  selected?: string,
   style?: CSSProperties,
-  mobileButtonType?: 'hamburger' | 'close',
-  onMobileButtonClick?: () => any,
   hideLogo?: boolean,
 }
 
@@ -24,49 +20,34 @@ interface Props {
  * 
  * @todo make tab list customizable, and add tests for it
  */
-export default function MobileNavigationBar({ selected, style, mobileButtonType, onMobileButtonClick, hideLogo }: Props) {
-  let button = (
-    <button
-      className={styles.menuContainer}
-      onClick={onMobileButtonClick}
-      tabIndex={0}
-    >
-      <Hamburger
-        width={24}
-        height={24}
-      />
-    </button>
-  );
+export default function MobileNavigationBar({ style, hideLogo }: Props) {
+  const [menuVisible, setMenuVisible] = useState(false);
 
-  if (mobileButtonType === 'close') {
-    button = (
-      <a
+  return (
+    <nav className={hideLogo ? `${utils.mobileNavContent} ${utils.mobileNavContentWithoutLogo}` : utils.mobileNavContent} style={style}>
+      <MobileNavigationMenu
+        visible={menuVisible}
+        onClose={() => setMenuVisible(false)}
+      />
+
+      <Link href="/" aria-label="Website Logo" className={styles.logoStandalone}>
+
+        <LogoStandalone
+          width={30.5}
+          height={30.5}
+        />
+
+      </Link>
+      <button
         className={styles.menuContainer}
-        href="#"
+        onClick={() => setMenuVisible(true)}
         tabIndex={0}
       >
-        <Exit
+        <Hamburger
           width={24}
           height={24}
         />
-      </a>
-    );
-  }
-
-  return (
-    <nav className={styles.container} style={style}>
-      <div className={hideLogo ? `${styles.contentContainer} ${styles.contentContainerWithoutLogo}` : styles.contentContainer}>
-        
-        <Link href="/" aria-label="Website Logo" className={styles.logoStandalone}>
-
-          <LogoStandalone
-            width={30.5}
-            height={30.5}
-          />
-
-        </Link>
-        {button}
-      </div>
+      </button>
     </nav>
   );
 }
