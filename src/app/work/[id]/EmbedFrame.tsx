cba2c9be2ecb9bc4c773b235350a3f46c878521a
Brendan Chen <smallpigeon1@gmail.com>
2023-06-20 15:19:29 -0700

    add embed frame component

---
+++ src/app/work/[id]/EmbedFrame.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { IframeHTMLAttributes, useState } from "react"
+import styles from './EmbedFrame.module.css'
+import ActionButton from "@/common/ActionButton"
+
+interface Props extends IframeHTMLAttributes<HTMLIFrameElement> {
+  src: string,
+  loadButtonText: string,
+  style?: React.CSSProperties,
+}
+
+export default function EmbedFrame(props: Props) {
+  const [shouldLoad, setShouldLoad] = useState(false);
+
+  return (
+    <div className={styles.container} style={props.style}>
+      {shouldLoad ? (
+        <iframe className={styles.iframe} src={props.src} />
+      ) : (
+        <div className={styles.buttonContainer}>
+          <ActionButton
+            text={props.loadButtonText}
+            onClick={() => setShouldLoad(true)}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
