32311d8202cdb88764a8778abade57908ee76cf7
Brendan Chen <smallpigeon1@gmail.com>
2023-06-20 15:31:34 -0700

    add background image support

--- src/app/work/[id]/EmbedFrame.tsx
+++ src/app/work/[id]/EmbedFrame.tsx
@@ -3,6 +3,7 @@
 import { IframeHTMLAttributes, useState } from "react"
 import styles from './EmbedFrame.module.css'
 import ActionButton from "@/common/ActionButton"
+import Image from "next/image";
 
 interface Props extends IframeHTMLAttributes<HTMLIFrameElement> {
   src: string,
@@ -23,12 +24,22 @@ export default function EmbedFrame(props: Props) {
           src={props.src}
         />
       ) : (
-        <div className={styles.buttonContainer}>
-          <ActionButton
-            text={props.loadButtonText}
-            onClick={() => setShouldLoad(true)}
-          />
-        </div>
+        <>
+          <div className={styles.buttonContainer}>
+            <ActionButton
+              text={props.loadButtonText}
+              onClick={() => setShouldLoad(true)}
+            />
+          </div>
+          <div className={styles.backgroundImageContainer}>
+            <Image
+              className={styles.backgroundImage}
+              src="https://images.unsplash.com/photo-1604890532358-4029426b27af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3474&q=80"
+              alt="Next.js logo"
+              fill
+            />
+          </div>
+        </>
       )}
     </div>
   )
