39b291bba34156470a00f7de2cfe0c605a9ffd93
Brendan Chen <smallpigeon1@gmail.com>
2023-06-19 17:15:26 -0700

    add tests for nav menu and button

---
+++ src/app/MobileNavigationMenu.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react"
+import MobileNavigationMenu, { MobileNavMenuButton } from "./MobileNavigationMenu"
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
+describe('MobileNavMenuButton', () => {
+  it('Renders a link to specifed href', () => {
+    render(<MobileNavMenuButton
+      text="Testing"
+      href="/test-link"
+    />);
+
+    const a = screen.getByRole('link');
+    expect(a).toHaveAttribute('href', '/test-link');
+  });
+
+  it('Renders the correct text', () => {
+    render(<MobileNavMenuButton
+      text="Testing"
+      href="/test-link"
+    />);
+
+    const text = screen.getByText("Testing");
+    expect(text).toBeInTheDocument();
+  });
+});
+
+describe('MobileNavigationMenu', () => {
+  it('Renders differently based on visibility', () => {
+    render(<>
+      <MobileNavigationMenu visible onClose={() => {}} />
+      <MobileNavigationMenu visible={false} onClose={() => {}} />
+    </>)
+
+    const menus = screen.getAllByTestId('mobileNavigationMenu')
+    expect(menus[0].classList).not.toStrictEqual(menus[1].classList)
+  })
+
+  it('Calls onClose prop', async () => {
+    const fn = jest.fn()
+    const user = userEvent.setup()
+    
+    render(
+      <>
+        <MobileNavigationMenu
+          onClose={fn}
+          visible
+        />
+      </>
+    )
+
+    const exitTitle = screen.getByTitle('Exit')
+    await user.click(exitTitle)
+    
+    expect(fn).toHaveBeenCalled()
+  })
+})
