b8a6e0191d4b79ea5d9313bb615e9e7dc87e20b8
Brendan Chen <smallpigeon1@gmail.com>
2023-06-21 12:03:21 -0700

    add configuration and helper functions

--- src/app/(document)/[id]/page.tsx
+++ src/app/(document)/[id]/page.tsx
@@ -1,10 +1,54 @@
-// export function getStaticParams() {
-//   // Fetch content from folder
+import fs from 'fs/promises'
+import path from 'path'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 
+/**
+ * Frontmatter used for the document page.
+ */
+interface Frontmatter {
+  title: string,
+  lastUpdated: string,
+  description: string,
+}
 
-//   // Return relevant IDs so they can be prerendered
+interface Document<T> {
+  frontmatter: T,
+  serialized: MDXRemoteSerializeResult,
+}
 
-// }
+/**
+ * Retrieve markdown data from the content folder.
+ * @param id ID of the given page.
+ */
+async function getDocument(id: string): Promise<Document<Frontmatter>> {
+  const filePath = path.join(process.cwd(), 'src', 'content', 'document', `${id}.mdx`)
+  const raw = await fs.readFile(filePath, 'utf-8')
+
+  // Serialize the MDX content and parse the frontmatter
+  const serialized = await serialize(raw, {
+    parseFrontmatter: true,
+  })
+
+  const frontmatter = serialized.frontmatter as unknown as Frontmatter
+
+  return {
+    frontmatter,
+    serialized,
+  }
+}
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  // Fetch content from folder
+  const files = await fs.readdir(path.join(process.cwd(), 'src', 'content', 'document'))
+
+  // Return relevant IDs so they can be prerendered
+  return files.map((file) => ({
+    id: file.split('.')[0],
+  }))
+}
 
 interface Props {
   params: {
@@ -12,7 +56,9 @@ interface Props {
   },
 }
 
-export default function Document({ params }: Props) {
+export default async function Document({ params }: Props) {
+  const { frontmatter, serialized } = await getDocument(params.id)
+
   return (
     <main>
       {/* Desktop sidebar */}
