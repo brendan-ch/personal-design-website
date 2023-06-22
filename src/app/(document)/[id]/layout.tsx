89848ced758e4c35507893ff8a03c9f4f941984b
Brendan Chen <smallpigeon1@gmail.com>
2023-06-22 09:55:27 -0700

    add styling for sidebar/table of contents

--- src/app/(document)/[id]/layout.tsx
+++ src/app/(document)/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import MDXSidebar from './MDXSidebar'
 import { getDocument } from './page'
+import styles from './layout.module.css'
 
 interface Props {
   params: {
@@ -15,11 +16,13 @@ export default async function DocumentLayout({ params, children }: Props) {
     // Get MDX content here and pass it down to sidebar
     // Render children as well
     <div>
-      <div>
+      <div className={styles.sidebar}>
         <a href="/">Back to Home</a>
 
         {/* to-do: pass content */}
-        <MDXSidebar source={serialized} />
+        <div className={styles.tableOfContents}>
+          <MDXSidebar source={serialized} />
+        </div>
       </div>
       <div>
         {children}
