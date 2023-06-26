e1ccab644541595f1c55c5a1f9d3e3b6397d551c
Brendan Chen <smallpigeon1@gmail.com>
2023-06-26 16:55:27 -0700

    add styling around mdx content

--- src/app/(default)/work/[id]/page.tsx
+++ src/app/(default)/work/[id]/page.tsx
@@ -1,9 +1,21 @@
+import styles from './page.module.css'
+import MDXContent from "@/app/(document)/[id]/MDXContent"
+import getWork from "./getWork"
+
 export const dynamicParams = false
 
-export default function Work() {
+interface Props {
+  params: {
+    id: string,
+  }
+}
+
+export default async function Work({ params }: Props) {
+  const { frontmatter, serialized } = await getWork(params.id)
+
   return (
-    <div>
-      <p>This is the Work page</p>
+    <div className={styles.container}>
+      <MDXContent source={serialized} />
     </div>
   )
 }
