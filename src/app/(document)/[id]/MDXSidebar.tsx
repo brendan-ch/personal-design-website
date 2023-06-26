b1ca2e94bdd3e7b6f8023e0e2f29d3f6a9511eb1
Brendan Chen <smallpigeon1@gmail.com>
2023-06-26 10:34:41 -0700

    add highlighted prop for heading

--- src/app/(document)/[id]/MDXSidebar.tsx
+++ src/app/(document)/[id]/MDXSidebar.tsx
@@ -5,39 +5,52 @@ import utils from '../../utils.module.css'
 
 interface MDXContentProps {
   source: MDXRemoteSerializeResult,
+  /**
+   * ID of the heading currently being highlighted.
+   */
+  highlightedId?: string,
 }
 
 const Nothing = () => <></>
 
 /**
- * Map of MDX components which map to React components.
- */
-const MDXComponents = {
-  h1: (props: React.HTMLProps<HTMLHeadingElement>) => {
-    const generatedLink = (props.children as string)
-      .toLowerCase()
-      .replaceAll(/[^A-Za-z0-9 ]/gi, '')
-      .replaceAll(' ', '-')
-
-    return (
-      <a href={`#${generatedLink}`} className={`${utils.monoText} ${utils.smallText}`}>
-        {props.children}
-      </a>
-    )
-  },
-  p: Nothing,
-  a: Nothing,
-  ul: Nothing,
-  ol: Nothing,
-  pre: Nothing,
-  img: Nothing,
-}
-
-/**
  * Sidebar renderer for the document page.
  * @param param0
  * @returns
- */
-export default function MDXSidebar({ source }: MDXContentProps) {
+*/
+export default function MDXSidebar({ source, highlightedId }: MDXContentProps) {
+  /**
+   * Map of MDX components which map to React components.
+   */
+  const MDXComponents = {
+    h1: (props: React.HTMLProps<HTMLHeadingElement>) => {
+      const generatedLink = (props.children as string)
+        .toLowerCase()
+        .replaceAll(/[^A-Za-z0-9 ]/gi, '')
+        .replaceAll(' ', '-')
+  
+      return (
+        <a
+          href={`#${generatedLink}`}
+          className={`${utils.monoText} ${utils.smallText}`}
+        >
+          {highlightedId === generatedLink ? (
+            <b>
+              {props.children}
+            </b>
+          ) : (
+            props.children
+          )}
+        </a>
+      )
+    },
+    p: Nothing,
+    a: Nothing,
+    ul: Nothing,
+    ol: Nothing,
+    pre: Nothing,
+    img: Nothing,
+  }
+
   return <MDXRemote {...source} components={MDXComponents} />
 }
