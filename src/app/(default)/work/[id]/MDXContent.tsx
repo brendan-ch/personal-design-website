41cacd205259040e7a401eed66ee3c3822ad28d9
Brendan Chen <smallpigeon1@gmail.com>
2023-06-26 17:28:40 -0700

    add imageSizes prop

--- src/app/(default)/work/[id]/MDXContent.tsx
+++ src/app/(default)/work/[id]/MDXContent.tsx
@@ -3,28 +3,41 @@
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
 import styles from './MDXContent.module.css'
 import Image from "next/image"
+import { ImageSize } from "./getWork"
 
 interface MDXContentProps {
   source: MDXRemoteSerializeResult,
+  imageSizes: ImageSize[],
 }
 
-/**
- * Map of MDX components which map to React components.
- */
-const MDXComponents = {
-  img: (props: React.HTMLProps<HTMLImageElement>) => props.src && props.alt ? (
-    <div className={styles.imageContainer}>
-      <Image src={props.src} alt={props.alt} fill />
-    </div>
-  ) : <></>,
-}
 
 /**
  * Content renderer for the Work page.
  * Unlike `MDXSidebar`, renders image content instead of headings.
  * @param param0
  * @returns
- */
-export default function MDXContent({ source }: MDXContentProps) {
+*/
+export default function MDXContent({ source, imageSizes }: MDXContentProps) {
+  /**
+   * Map of MDX components which map to React components.
+   */
+  const MDXComponents = {
+    img: (props: React.HTMLProps<HTMLImageElement>) => {
+      if (!props.src || !props.alt) {
+        return <></>
+      }
+
+      const dimensions = imageSizes.find(({ imagePath }) => imagePath === props.src)
+
+      return (
+        <div className={styles.imageContainer} style={{
+          aspectRatio: dimensions ? `${dimensions.width} / ${dimensions.height}` : '3 / 2'
+        }}>
+          <Image src={props.src} alt={props.alt} fill />
+        </div>
+      )
+    },
+  }
+
   return <MDXRemote {...source} components={MDXComponents} />
 }
--- src/app/(default)/work/[id]/page.tsx
+++ src/app/(default)/work/[id]/page.tsx
@@ -1,6 +1,6 @@
 import styles from './page.module.css'
-import MDXContent from "@/app/(document)/[id]/MDXContent"
 import getWork from "./getWork"
+import MDXContent from './MDXContent'
 
 export const dynamicParams = false
 
@@ -11,11 +11,11 @@ interface Props {
 }
 
 export default async function Work({ params }: Props) {
-  const { frontmatter, serialized } = await getWork(params.id)
+  const { frontmatter, serialized, imageSizes } = await getWork(params.id)
 
   return (
     <div className={styles.container}>
-      <MDXContent source={serialized} />
+      <MDXContent source={serialized} imageSizes={imageSizes} />
     </div>
   )
 }
