de67374d340b6ee821b52e642ddab52fc277ff61
Brendan Chen <smallpigeon1@gmail.com>
2023-06-29 10:23:00 -0700

    test out preview image display

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,16 +1,31 @@
 import { Metadata } from 'next'
 import styles from './page.module.css'
 import Link from 'next/link'
+import getWork from './work/[id]/getWork'
+import GalleryItem from './GalleryItem'
 
 export const metadata: Metadata = {
   title: 'Featured Works',
   description: 'Featured works on the design.bchen.dev website.'
 }
 
-export default function Home() {
+export default async function Home() {
+  const { frontmatter, previewImageSize } = await getWork('headspace-logo-redesign')
+
   return (
     <main className={styles.main}>
       <Link href="/work/headspace-logo-redesign">Headspace Logo Redesign</Link>
+      <GalleryItem
+        title={frontmatter.title}
+        date={frontmatter.date}
+        imageSrc={frontmatter.previewImage!}
+        imageAlt="Headspace Logo Redesign"
+        imageSize={previewImageSize ? previewImageSize : {
+          height: '1000',
+          width: '2000',
+          imagePath: frontmatter.previewImage!
+        }}
+      />
       <p>Some page content</p>
       <p>Some page content</p>
       <p>Some page content</p>
