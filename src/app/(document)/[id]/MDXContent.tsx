e2dca4848baf691604d1fa7d5a519664c03c4082
Brendan Chen <smallpigeon1@gmail.com>
2023-06-29 09:53:05 -0700

    remove lazy loading

--- src/app/(document)/[id]/MDXContent.tsx
+++ src/app/(document)/[id]/MDXContent.tsx
@@ -66,5 +66,5 @@ const MDXComponents = {
  * @returns
  */
 export default function MDXContent({ source }: MDXContentProps) {
-  return <MDXRemote {...source} components={MDXComponents} lazy />
+  return <MDXRemote {...source} components={MDXComponents} />
 }
