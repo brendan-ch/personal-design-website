e9f157b26edd807e7a381d1f8050970aa5285f8d
Brendan Chen <smallpigeon1@gmail.com>
2023-07-03 11:22:59 -0700

    Revert "get compiled content"
    This reverts commit 91b6521942f94211d10d4b7fbb0d8b3ba50adcc8.

--- src/app/(default)/work/[id]/getPrecompiledWork.ts
+++ src/app/(default)/work/[id]/getPrecompiledWork.ts
@@ -4,29 +4,14 @@ import { cache } from "react"
 import { compileMDX } from 'next-mdx-remote/rsc'
 import { Frontmatter, ImageSize } from './getWork'
 
-type Components = {
-  [key: string]: any,
-}
-
 /**
  * Raw data and image size data for a work.
  */
-export interface PrecompiledWork<T> {
+export interface PrecompiledWork {
   /**
    * Raw string containing frontmatter and markdown.
    */
   raw: string,
-
-  /**
-   * 
-   */
-  content: React.ReactNode,
-
-  /**
-   * Object containing the frontmatter of the work.
-   */
-  frontmatter: T,
-
   /**
    * Array of image sizes corresponding to the raw content.
    */
@@ -37,21 +22,15 @@ export interface PrecompiledWork<T> {
   previewImageSize: ImageSize,
 }
 
-const getFile = cache(async (id: string) => {
-  const filePath = path.join(process.cwd(), 'src', 'content', 'work', `${id}.mdx`)
-  const raw = await fs.readFile(filePath, 'utf-8')
-
-  return raw
-})
-
 /**
  * Retrieve raw data from the content folder, as well as image data.
  * Results from this function are cached and deduplicated; the same call
  * twice will return the cached result.
  * @param id ID of the given page.
  */
-const getPrecompiledWork = cache(async (id: string, components?: Components): Promise<PrecompiledWork<Frontmatter>> => {
-  const raw = await getFile(id)
+const getPrecompiledWork = cache(async (id: string): Promise<PrecompiledWork> => {
+  const filePath = path.join(process.cwd(), 'src', 'content', 'work', `${id}.mdx`)
+  const raw = await fs.readFile(filePath, 'utf-8')
 
   // Serialize the MDX content and parse the frontmatter
   const compiled = await compileMDX({
@@ -59,7 +38,6 @@ const getPrecompiledWork = cache(async (id: string, components?: Components): Pr
     options: {
       parseFrontmatter: true,
     },
-    components,
   })
 
   const frontmatter = compiled.frontmatter as unknown as Frontmatter
@@ -71,8 +49,6 @@ const getPrecompiledWork = cache(async (id: string, components?: Components): Pr
   const { allImages, previewImageSize } = parsedImageSizeData.work.find((item: any) => item.id === id)
 
   return {
-    content: compiled.content,
-    frontmatter,
     imageSizes: allImages ? allImages : [],
     previewImageSize,
     raw,
