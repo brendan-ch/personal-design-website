97ce437673cc1553fd44197427749144f7d32b12
Brendan Chen <smallpigeon1@gmail.com>
2023-07-06 11:45:00 -0700

    add project to homepage

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: 'Featured works on the design.bchen.dev website.'
 }
 
-const column1 = ['the-birds', 'uhs-planner-cover']
+const column1 = ['cmes-admin-panel', 'the-birds', 'uhs-planner-cover']
 const column2 = ['headspace-logo-redesign', 'spirit-week-poster', 'abstract-color-art']
 
 const columns = [column1, column2]
