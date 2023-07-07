c3cabc2aaed785fd7a36116bee42090d8bd5f1bb
Brendan Chen <smallpigeon1@gmail.com>
2023-07-07 11:33:59 -0700

    add not-found error boundary

---
+++ src/app/(document)/[...not-found]/page.ts
@@ -0,0 +1,8 @@
+import { notFound } from "next/navigation";
+
+// See this issue for how this works:
+// https://github.com/vercel/next.js/issues/49281
+
+export default function NotFound() {
+  notFound()
+}
