cf87cc58e81d6707bf7b80c9d2e614ae6efc0577
Brendan Chen <smallpigeon1@gmail.com>
2023-07-07 12:18:55 -0700

    remove metadata export

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -5,11 +5,6 @@ import Image from 'next/image'
 import { Metadata } from 'next'
 import Head from 'next/head'
 
-export const metadata: Metadata = {
-  title: 'Page Not Found',
-  description: 'We couldn\'t find the page you were looking for.',
-}
-
 export default function ServerError() {
   const capybara = 'https://images.unsplash.com/photo-1595017013941-cab3d4c8d02f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3570&q=80'
 
