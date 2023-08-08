2babd4fb81828ca4a324819eae2a7b92b8f0074d
Brendan Chen <smallpigeon1@gmail.com>
2023-08-08 10:55:20 -0700

    add XML generation

---
+++ src/app/(default)/sitemap.xml/route.ts
@@ -0,0 +1,67 @@
+import { NextRequest, NextResponse } from "next/server";
+import path from 'path'
+import fs from 'fs/promises'
+
+/**
+ * Retrieve all available IDs in the works folder.
+ * @returns
+ */
+async function getWorkIds() {
+  const filePath = path.join(process.cwd(), 'src', 'content', 'work')
+  const ids = await fs.readdir(filePath)
+  return ids.map((id) => id.split('.mdx')[0])
+}
+
+/**
+ * Retrieve all available IDs in the documents folder.
+ * @returns
+ */
+async function getDocumentIds() {
+  const filePath = path.join(process.cwd(), 'src', 'content', 'document')
+  const ids = await fs.readdir(filePath)
+  return ids.map((id) => id.split('.mdx')[0])
+}
+
+export async function GET(req: NextRequest) {
+  const baseUrl = `https://${req.headers.get('host')}`
+  console.log(baseUrl)
+
+  const workPages = await getWorkIds()
+  const docPages = await getDocumentIds()
+  console.log(workPages)
+  console.log(docPages)
+
+  const highPriority = [
+    '',
+  ]
+
+  // Generate the sitemap here
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${highPriority.map((value) => {
+      return `<url>
+        <loc>${baseUrl}${value}</loc>
+        <priority>1.0</priority>
+      </url>`;
+    }).join('')}
+    ${workPages.map((value) => {
+      return `<url>
+        <loc>${baseUrl}/work/${value}</loc>
+        <priority>0.5</priority>
+      </url>`;
+    }).join('')}
+    ${docPages.map((value) => {
+      return `<url>
+        <loc>${baseUrl}/${value}</loc>
+        <priority>0.25</priority>
+      </url>`;
+    }).join('')}
+  </urlset>`
+  console.log(sitemap)
+
+  const text = new NextResponse(sitemap)
+  text.headers.set('Content-Type', 'application/xml')
+  console.log(text)
+
+  return text
+}
