4fdbe35752792c4bd10b6cb9a1a05ce3c18cc42a
Brendan Chen <smallpigeon1@gmail.com>
2023-08-10 11:26:46 -0700

    add clockwise page to homepage

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
 }
 
-const column1 = ['cmes-admin-panel', 'the-birds', 'uhs-planner-cover']
+const column1 = ['cmes-admin-panel', 'clockwise', 'the-birds', 'uhs-planner-cover']
 const column2 = ['headspace-logo-redesign', 'standard-catalog', 'spirit-week-poster', 'abstract-color-art']
 
 const columns = [column1, column2]
