4051f586746b866055bde808e17a58bb5f7342ce
Brendan Chen <smallpigeon1@gmail.com>
2023-08-16 10:41:20 -0700

    use HomeContent component

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from 'next'
-import styles from './page.module.css'
-import utils from '../utils.module.css'
-import GalleryItem from './GalleryItem'
 import getWorks from './work/[id]/getWorks'
+import HomeContent from './HomeContent'
 
 export const metadata: Metadata = {
   title: 'Design by Brendan Chen',
@@ -31,31 +29,6 @@ export default async function Home() {
   }))
 
   return (
-    <main className={styles.main}>
-      <div className={styles.heading}>
-        <h1 className={utils.h0Text}>Featured Works</h1>
-        <p className={`${utils.monoText} ${utils.smallText}`}>by Brendan Chen</p>
-      </div>
-      {/* Wrap the columns with a div */}
-      <div className={styles.columns}>
-        {workColumns.map((column, i) => (
-          <div className={styles.column} key={i}>
-            {column.map(({ frontmatter, previewImageSize }, j) => (
-              // @ts-ignore Server Component
-              <GalleryItem
-                title={frontmatter.title}
-                date={frontmatter.date}
-                imageSrc={frontmatter.previewImage}
-                imageAlt={`Preview image for ${frontmatter.title}`}
-                imageSize={previewImageSize}
-                key={j}
-                href={`/work/${columns[i][j]}`}
-                description={frontmatter.description}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-    </main>
+    <HomeContent columns={workColumns} />
   )
 }
