2032e985cb2f5a30c67504bab79ae6e2859e3970
Brendan Chen <smallpigeon1@gmail.com>
2023-08-16 11:39:55 -0700

    add graphic design page

---
+++ src/app/(default)/graphic-design/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next";
+import getWorks from "../work/[id]/getWorks";
+import HomeContent from "../HomeContent";
+
+
+export const metadata: Metadata = {
+  title: 'UI/UX Design by Brendan Chen',
+  description: "I'm Brendan, a designer, developer, and college student living in Orange, California. This website is a creative outlet for me to showcase my design works.",
+  openGraph: {
+    title: 'UI/UX Design by Brendan Chen',
+    description: "I'm Brendan, a designer, developer, and college student living in Orange, California. This website is a creative outlet for me to showcase my design works.",
+    images: [
+      {
+        width: 1200,
+        height: 630,
+        url: '/static/og-featured-works.png',
+      },
+    ],
+  },
+}
+
+const column1 = ['uhs-planner-cover', 'the-birds']
+const column2 = ['headspace-logo-redesign', 'spirit-week-poster', 'abstract-color-art']
+
+const columns = [column1, column2]
+
+export default async function GraphicDesign() {
+  const workColumns = await Promise.all(columns.map(async (column) => {
+    return await getWorks(column)
+  }))
+
+  return (
+    <HomeContent headline="Graphic Design" columns={workColumns} />
+  )
+}
