bd086581b3a3c699315dd88d979c6cfc6c42b95c
Brendan Chen <smallpigeon1@gmail.com>
2023-08-29 13:53:54 -0700

    swap standard catalog/headspace logo redesign

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 }
 
 const column1 = ['cmes-admin-panel', 'clockwise', 'uhs-planner-cover']
-const column2 = ['headspace-logo-redesign', 'standard-catalog', 'spirit-week-poster']
+const column2 = ['standard-catalog', 'headspace-logo-redesign', 'spirit-week-poster']
 
 const columns = [column1, column2]
 
