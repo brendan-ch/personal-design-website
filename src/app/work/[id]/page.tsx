26efb00bcbfbd3c647e25046d5da4df1e9705dd5
Brendan Chen <smallpigeon1@gmail.com>
2023-09-06 22:02:59 -0700

    disable static generation

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -2,22 +2,10 @@ import fs from 'fs/promises'
 import path from 'path'
 import styles from './page.module.css'
 
-export const dynamicParams = false
-
 interface Props {
   params: {
     id: string,
   }
-}
-
-export async function generateStaticParams() {
-  // Fetch content from folder
-  const files = await fs.readdir(path.join(process.cwd(), 'src', 'content', 'work'))
-
-  // Return relevant IDs so they can be prerendered
-  return files.map((file) => ({
-    id: file.split('.')[0],
-  }))
 }
 
 export default async function Work({ params }: Props) {
