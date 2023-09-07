1b875678f2dc6d5ef7bafd50469c6666d417c3ae
Brendan Chen <smallpigeon1@gmail.com>
2023-09-07 15:41:38 -0700

    fix import + remove static generation

--- src/app/(default)/@modal/(.)work/[id]/page.tsx
+++ src/app/(default)/@modal/(.)work/[id]/page.tsx
@@ -1,24 +1,12 @@
 import fs from 'fs/promises'
 import path from 'path'
 import styles from '../../../work/[id]/page.module.css'
-import MDXContent from '@/app/(document)/[id]/MDXContent'
-
-export const dynamicParams = false
+import MDXContent from '@/app/(default)/work/[id]/MDXContent'
 
 interface Props {
   params: {
     id: string,
   }
-}
-
-export async function generateStaticParams() {
-  // Fetch content from folder
-  const files = await fs.readdir(path.join(process.cwd(), 'src', 'content', 'work'))
-
-  // Return relevant IDs so they can be prerendered
-  return files.map((file) => ({
-    id: file.split('.')[0],
-  }))
 }
 
 export default async function Work({ params }: Props) {
