4c020a86c7d7af1e0e8b621d20ed4b0f87849c25
Brendan Chen <smallpigeon1@gmail.com>
2023-09-07 15:01:48 -0700

    add and apply intercepted work route

---
+++ src/app/(default)/@modal/(.)work/[id]/page.tsx
@@ -0,0 +1,31 @@
+import fs from 'fs/promises'
+import path from 'path'
+import styles from '../../../work/[id]/page.module.css'
+import MDXContent from '@/app/(document)/[id]/MDXContent'
+
+export const dynamicParams = false
+
+interface Props {
+  params: {
+    id: string,
+  }
+}
+
+export async function generateStaticParams() {
+  // Fetch content from folder
+  const files = await fs.readdir(path.join(process.cwd(), 'src', 'content', 'work'))
+
+  // Return relevant IDs so they can be prerendered
+  return files.map((file) => ({
+    id: file.split('.')[0],
+  }))
+}
+
+export default async function Work({ params }: Props) {
+  return (
+    <div className={styles.container}>
+      {/* @ts-ignore Server Component */}
+      <MDXContent id={params.id} />
+    </div>
+  )
+}
