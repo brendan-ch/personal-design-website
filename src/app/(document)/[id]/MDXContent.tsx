7d98b6619cda613eb0476aa6eaade8a7922b6cae
Brendan Chen <smallpigeon1@gmail.com>
2023-09-06 22:42:16 -0700

    Revert "remove grouping of routes"
    This reverts commit c504b84c4a15138804078cefad5f3ea8568fbe98.

---
+++ src/app/(document)/[id]/MDXContent.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import Anchor from "@/common/Anchor"
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
+import styles from './MDXContent.module.css'
+import { BACKGROUND, WHITE } from "@/app/Constants"
+import Image from "next/image"
+import generateHeadingLink from "@/helpers/generateHeadingLink"
+
+interface MDXContentProps {
+  source: MDXRemoteSerializeResult,
+}
+
+/**
+ * Map of MDX components which map to React components.
+ */
+const MDXComponents = {
+  h1: (props: React.HTMLProps<HTMLHeadingElement>) => {
+    const generatedLink = generateHeadingLink(props.children as string)
+
+    return (
+      <div className={styles.anchorLinkWrapper}>
+        {/* anchorWrapper is required for heading links to work
+        in a performant manner */}
+        {/* see useEffect hook in MDXSidebar component */}
+        <div className={`${styles.anchorWrapper} anchorWrapper`} id={generatedLink}>
+          <Anchor
+            text={`${props.children}`}
+            // id={generatedLink}
+            style={{
+              borderTopColor: BACKGROUND,
+            }}
+          />
+        </div>
+      </div>
+    )
+  },
+  p: (props: React.HTMLProps<HTMLParagraphElement>) => (
+    <p className={styles.text} {...props}></p>
+  ),
+  ol: (props: React.HTMLProps<HTMLOListElement>) => (
+    <ol className={`${styles.list} ${styles.orderedList}`}>
+      {props.children}
+    </ol>
+  ),
+  ul: (props: React.HTMLProps<HTMLUListElement>) => (
+    <ul className={styles.list}>
+      {props.children}
+    </ul>
+  ),
+  pre: (props: React.HTMLProps<HTMLPreElement>) => (
+    <div className={styles.preWrapper}>
+      <pre className={styles.pre} {...props}>
+      </pre>
+    </div>
+  ),
+  img: (props: React.HTMLProps<HTMLImageElement>) => props.src && props.alt ? (
+    <div className={styles.imageContainer}>
+      <Image src={props.src} alt={props.alt} fill />
+    </div>
+  ) : <></>,
+  a: (props: React.HTMLProps<HTMLAnchorElement>) => (
+    <a target="_blank" rel="noreferrer" {...props}>
+    </a>
+  )
+}
+
+/**
+ * Content renderer for the Document page.
+ * Unlike `MDXSidebar`, renders actual content instead of just headings.
+ * @param param0
+ * @returns
+ */
+export default function MDXContent({ source }: MDXContentProps) {
+  return <MDXRemote {...source} components={MDXComponents} />
+}
---
+++ src/app/(document)/[id]/MDXSidebar.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { MDXRemoteSerializeResult } from "next-mdx-remote/rsc"
+import utils from '../../utils.module.css'
+import generateHeadingLink from "@/helpers/generateHeadingLink"
+import { MDXRemote } from "next-mdx-remote"
+import useScrollHighlight from "@/hooks/useScrollHighlight"
+
+interface MDXContentProps {
+  source: MDXRemoteSerializeResult,
+}
+
+/**
+ * Offset from the viewport's top for a link to be highlighted.
+ */
+const HIGHLIGHT_TOP_MARGIN = 320
+
+const Nothing = () => <></>
+
+/**
+ * Sidebar renderer for the document page.
+ * @param param0
+ * @returns
+*/
+export default function MDXSidebar({ source }: MDXContentProps) {
+  const highlighted = useScrollHighlight('anchorWrapper', HIGHLIGHT_TOP_MARGIN)
+    
+  /**
+   * Map of MDX components which map to React components.
+   */
+  const MDXComponents = {
+    h1: (props: React.HTMLProps<HTMLHeadingElement>) => {
+      const generatedLink = generateHeadingLink(props.children as string)
+  
+      return (
+        <a
+          href={`#${generatedLink}`}
+          className={`${utils.monoText} ${utils.smallText}`}
+        >
+          {highlighted === generatedLink ? (
+            <b>
+              {props.children}
+            </b>
+          ) : (
+            props.children
+          )}
+          {/* {props.children} */}
+        </a>
+      )
+    },
+    p: Nothing,
+    a: Nothing,
+    ul: Nothing,
+    ol: Nothing,
+    pre: Nothing,
+    img: Nothing,
+    hr: Nothing,
+    EmbedFrame: Nothing,
+    blockquote: Nothing,
+  }
+
+  return <MDXRemote components={MDXComponents} {...source} />
+}
