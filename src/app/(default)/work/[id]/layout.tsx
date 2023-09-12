1ac2029852dc62b294b4929dad06fe1d0ec3a175
Brendan Chen <smallpigeon1@gmail.com>
2023-09-11 21:20:33 -0700

    Revert "use parallel routing only, not intercepted route"
    This reverts commit babbc588203ae381a31e39a11a73c8233ccd1415.

---
+++ src/app/(default)/work/[id]/layout.tsx
@@ -0,0 +1,42 @@
+import getWork from './getWork'
+import { Metadata, ResolvingMetadata } from 'next'
+import LayoutContent from './LayoutContent'
+
+interface LayoutProps {
+  params: {
+    id: string,
+  },
+  children: React.ReactNode,
+}
+
+export async function generateMetadata(
+  { params }: LayoutProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { frontmatter } = await getWork(params.id)
+  
+  return {
+    title: frontmatter.title,
+    description: frontmatter.description,
+    openGraph: {
+      images: frontmatter.ogImage ? {
+        url: frontmatter.ogImage,
+        width: 1200,
+        height: 630,
+      } : undefined,
+    },
+    // TO-DO: dynamically generate image based on page
+  }
+}
+
+export default async function WorkLayout({ children, params }: LayoutProps) {
+  return (
+    // @ts-ignore Server Component
+    <LayoutContent
+      id={params.id}
+      goBackOnExit={false}
+    >
+      {children}
+    </LayoutContent>
+  )
+}
