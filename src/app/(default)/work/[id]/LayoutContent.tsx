07d1814e7a381f581cf4ce669280168b04e0b9f8
Brendan Chen <smallpigeon1@gmail.com>
2023-10-22 16:19:36 -0700

    add suspense fallback for layout content

--- src/app/(default)/work/[id]/LayoutContent.tsx
+++ src/app/(default)/work/[id]/LayoutContent.tsx
@@ -5,6 +5,7 @@ import MDXSidebar from '@/app/(document)/[id]/MDXSidebar'
 import ShareCTA, { PageExternalLink } from './ShareCTA'
 import getWork from './getWork'
 import ExitButton from './ExitButton'
+import { CSSProperties, Suspense } from 'react'
 
 interface Props {
   children: React.ReactNode,
@@ -19,6 +20,85 @@ interface Props {
    * By default it returns the user to the home page.
    */
   goBackOnExit?: boolean,
+}
+
+function LoadingSkeletonBar({ style }: {
+  style: CSSProperties,
+}) {
+  return (
+    <div className={styles.loadingSkeletonBar} style={style}>
+    </div>
+  )
+}
+
+function LayoutContentLoading() {
+  return (
+    <div className={`${utils.maxWidthWrapper} ${styles.contentFadeIn}`}>
+      <div className={styles.leftSidebar}>
+        {/* MDX sidebar with header links */}
+        <div className={styles.tableOfContents}>
+          <LoadingSkeletonBar style={{
+            width: 200,
+            height: 20,
+          }} />
+          <LoadingSkeletonBar style={{
+            width: 200,
+            height: 20,
+          }} />
+        </div>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        paddingTop: 96,
+        paddingBottom: 144,
+        gap: 2
+      }}>
+        <div className="workIntercepted"></div>
+        <LoadingSkeletonBar style={{
+          height: 800,
+        }} />
+        <LoadingSkeletonBar style={{
+          height: 800,
+        }} />
+      </div>
+      <div className={styles.rightSidebar}>
+        {/* Title and description based on frontmatter */}
+        <div className={styles.descriptionContainer}>
+          <div className={styles.titleContainer}>
+            <LoadingSkeletonBar style={{
+              width: 200,
+              height: 20,
+            }} />
+            <LoadingSkeletonBar style={{
+              width: 200,
+              height: 20,
+            }} />
+          </div>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 4,
+          }}>
+            <LoadingSkeletonBar style={{
+              height: 16,
+            }} />
+            <LoadingSkeletonBar style={{
+              height: 16,
+            }} />
+            <LoadingSkeletonBar style={{
+              width: 160,
+              height: 16,
+            }} />
+          </div>
+        </div>
+        {/* Share CTA */}
+        <div className={styles.shareContainer}>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 /**
@@ -46,43 +126,45 @@ export default async function LayoutContent({
       url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(currentPath)}`
     },
   ]
-  
+
   return (
     <div className={styles.container}>
-      <div className={`${utils.maxWidthWrapper} ${styles.contentFadeIn}`}>
-        {/* Exit button, positioned relative to container */}
-        <ExitButton goBackOnExit={goBackOnExit} />
-        <div className={styles.leftSidebar}>
-          {/* MDX sidebar with header links */}
-          <div className={styles.tableOfContents}>
-            <MDXSidebar source={serialized} />
-          </div>
-        </div>
-        <div className={styles.shareContainerMobile}>
-          <ShareCTA
-            copyLink={currentPath}
-            links={sharingLinks}
-          />
-        </div>
-        {children}
-        <div className={styles.rightSidebar}>
-          {/* Title and description based on frontmatter */}
-          <div className={styles.descriptionContainer}>
-            <div className={styles.titleContainer}>
-              <p className={`${utils.monoText} ${utils.smallText}`}>{frontmatter.title}</p>
-              <p className={`${utils.monoText} ${utils.smallText} ${styles.date}`}>{frontmatter.date}</p>
+      <Suspense fallback={<LayoutContentLoading />}>
+        <div className={`${utils.maxWidthWrapper} ${styles.contentFadeIn}`}>
+          {/* Exit button, positioned relative to container */}
+          <ExitButton goBackOnExit={goBackOnExit} />
+          <div className={styles.leftSidebar}>
+            {/* MDX sidebar with header links */}
+            <div className={styles.tableOfContents}>
+              <MDXSidebar source={serialized} />
             </div>
-            <p className={utils.smallText}>{frontmatter.description}</p>
           </div>
-          {/* Share CTA */}
-          <div className={styles.shareContainer}>
+          <div className={styles.shareContainerMobile}>
             <ShareCTA
               copyLink={currentPath}
               links={sharingLinks}
             />
           </div>
+          {children}
+          <div className={styles.rightSidebar}>
+            {/* Title and description based on frontmatter */}
+            <div className={styles.descriptionContainer}>
+              <div className={styles.titleContainer}>
+                <p className={`${utils.monoText} ${utils.smallText}`}>{frontmatter.title}</p>
+                <p className={`${utils.monoText} ${utils.smallText} ${styles.date}`}>{frontmatter.date}</p>
+              </div>
+              <p className={utils.smallText}>{frontmatter.description}</p>
+            </div>
+            {/* Share CTA */}
+            <div className={styles.shareContainer}>
+              <ShareCTA
+                copyLink={currentPath}
+                links={sharingLinks}
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      </Suspense>
     </div>
   )
 }
