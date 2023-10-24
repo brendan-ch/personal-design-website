9bb257ed07eb09550f6c90efaedd4d74c8be145e
Brendan Chen <smallpigeon1@gmail.com>
2023-10-24 15:34:58 -0700

    make loading skeleton bar common component

--- src/app/(default)/work/[id]/loading.tsx
+++ src/app/(default)/work/[id]/loading.tsx
@@ -1,15 +1,5 @@
 import styles from './loading.module.css'
-import utils from '../../../utils.module.css'
-import { CSSProperties } from 'react'
-
-function LoadingSkeletonBar({ style }: {
-  style: CSSProperties,
-}) {
-  return (
-    <div className={styles.loadingSkeletonBar} style={style}>
-    </div>
-  )
-}
+import LoadingSkeletonBar from '@/common/LoadingSkeletonBar'
 
 /**
  * Display a loading skeleton for the page content inside the layout.
