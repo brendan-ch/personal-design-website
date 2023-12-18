cdf5beff5ff4c27af63d1a5c8160b8b51a8fefb2
Brendan Chen <smallpigeon1@gmail.com>
2023-12-18 03:39:13 +0000

    use adjacent sibling selector

--- src/app/(default)/work/[id]/MDXContent.tsx
+++ src/app/(default)/work/[id]/MDXContent.tsx
@@ -56,7 +56,7 @@ export default async function MDXContent({ id }: MDXContentProps) {
       const generatedLink = generateHeadingLink(props.children as string)
 
       // Return empty div which can be referred to by ID
-      return <div id={generatedLink} className="anchorWrapper"></div>
+      return <div id={generatedLink} className={`${styles.anchorWrapper} anchorWrapper`}></div>
     },
     h2: Nothing,
     h3: Nothing,
