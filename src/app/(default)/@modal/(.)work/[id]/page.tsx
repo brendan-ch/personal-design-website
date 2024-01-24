d86d6f497184baca47a088cf2a6e4f7399eac12e
Brendan Chen <me@bchen.dev>
2024-01-24 12:34:32 -0800

    add intercepted page documentation

--- src/app/(default)/@modal/(.)work/[id]/page.tsx
+++ src/app/(default)/@modal/(.)work/[id]/page.tsx
@@ -1,14 +1,20 @@
-import fs from 'fs/promises'
-import path from 'path'
 import styles from '../../../work/[id]/page.module.css'
 import MDXContent from '@/app/(default)/work/[id]/MDXContent'
 
 interface Props {
   params: {
+    /**
+     * The ID (filename) of the work to render.
+     */
     id: string,
   }
 }
 
+/**
+ * Primary content rendered for the intercepted route for the work page.
+ * @param param0
+ * @returns
+ */
 export default async function Work({ params }: Props) {
   return (
     <div className={styles.container}>
