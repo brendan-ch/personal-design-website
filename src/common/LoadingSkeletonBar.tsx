e8215918c44ecef459a33038d0ddb273b865101c
Brendan Chen <me@bchen.dev>
2024-01-24 12:49:44 -0800

    add LoadingSkeletonBar documentation

--- src/common/LoadingSkeletonBar.tsx
+++ src/common/LoadingSkeletonBar.tsx
@@ -1,7 +1,15 @@
 import styles from './LoadingSkeletonBar.module.css'
 import { CSSProperties } from "react";
 
+/**
+ * Loading skeleton.
+ * @param param0
+ * @returns
+ */
 export default function LoadingSkeletonBar({ style }: {
+  /**
+   * Custom styling for the loading skeleton.
+   */
   style: CSSProperties,
 }) {
   return (
