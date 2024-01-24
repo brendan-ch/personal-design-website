9d99b4a1413902673bf45e37765a63403a6d4795
Brendan Chen <me@bchen.dev>
2024-01-24 12:28:25 -0800

    add HorizontalWrapper component documentation

--- src/app/(default)/work/[id]/mdx/HorizontalWrapper.tsx
+++ src/app/(default)/work/[id]/mdx/HorizontalWrapper.tsx
@@ -1,5 +1,11 @@
 import styles from './HorizontalWrapper.module.css'
 
+/**
+ * MDX component which groups elements horizontally on desktop,
+ * and vertically on mobile.
+ * @param props
+ * @returns
+ */
 export default function HorizontalWrapper(props: React.HTMLProps<HTMLDivElement>) {
   return (
     <div className={styles.horizontalWrapper} {...props}>
