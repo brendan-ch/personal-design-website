867ab13b45a09a909461ac5b5eb3e6d55a567403
Brendan Chen <me@bchen.dev>
2024-01-24 12:33:35 -0800

    add intercepted route layout documentation

--- src/app/(default)/@modal/(.)work/[id]/layout.tsx
+++ src/app/(default)/@modal/(.)work/[id]/layout.tsx
@@ -7,6 +7,12 @@ interface LayoutProps {
   children: React.ReactNode,
 }
 
+/**
+ * Layout rendered when the route is intercepted
+ * (i.e. when the user clicks on a project from the home page)
+ * @param param0
+ * @returns
+ */
 export default async function WorkLayout({ children, params }: LayoutProps) {
   return (
     // @ts-ignore Server Component
